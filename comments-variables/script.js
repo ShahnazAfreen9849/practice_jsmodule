22f5bb571a519c2604b4e6a401971f86a39896cb
Shanzzz <shahnazafreen9849@gmail.com>
2023-05-18 23:50:59 -0400

    created repo, worked on 2,4,6,8 activity

---
+++ comments-variables/script.js
@@ -0,0 +1,29 @@
+//comment
+/* paragraph comment
+*/
+
+console.log("External JavaScript is linked");
+
+
+//Declare variables using var
+
+var personName = 'Tony';
+var pets = 3;
+var funFact = 'I like pineapple';
+
+//log data and variable name
+
+console.log('I am ' + personName);
+console.log('I have ' + pets + ' and I love them');
+console.log(funFact);
+
+
+//reasigning 
+personName = "Devika";
+pets = 6;
+funFact = "I like pets";
+
+
+console.log(personName);
+console.log(pets);
+console.log(funFact);
